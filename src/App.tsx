f6249bbe44f145bb8c3f9d174e82db6295a2a391
Erikharmens <54328933+Erikharmens@users.noreply.github.com>
2022-07-25 18:02:04 +0200

    config, tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,7 +16,7 @@ function App() {
       </div>
       <h1>Foodie Collection Inc</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button className="bg-blue-400" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
