f70cc55ca1ee0eb1d2b4702012b0dc94136eaba5
Erikharmens <54328933+Erikharmens@users.noreply.github.com>
2022-08-10 20:45:13 +0200

    testing with essential ingredients checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,98 @@ function App() {
     <>
       <Navbar />
       <div className="container mx-auto py-8 px-8 mt-24 rounded-md bg-blue-100">
+        <h1 className="font-bold text-xl pb-8">
+          Essential cooking ingredients
+        </h1>
+        <h1 className="font-bold text-2x1 pb-8">I have these available:</h1>
+
+        <label class="inline-flex relative items-center mb-4 cursor-pointer">
+          <input
+            type="checkbox"
+            value=""
+            id="default-toggle"
+            className="sr-only peer"
+          />
+          <div className="w-11 h-6 bg-gray-800 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+          <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+            Water
+          </span>
+        </label>
+        <br />
+        <label class="inline-flex relative items-center mb-4 cursor-pointer">
+          <input
+            type="checkbox"
+            value=""
+            id="default-toggle"
+            className="sr-only peer"
+          />
+          <div className="w-11 h-6 bg-gray-800 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+          <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+            Salt
+          </span>
+        </label>
+        <br />
+        <label class="inline-flex relative items-center mb-4 cursor-pointer">
+          <input
+            type="checkbox"
+            value=""
+            id="default-toggle"
+            className="sr-only peer"
+          />
+          <div className="w-11 h-6 bg-gray-800 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+          <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+            Pepper
+          </span>
+        </label>
+        <br />
+        <label class="inline-flex relative items-center mb-4 cursor-pointer">
+          <input
+            type="checkbox"
+            value=""
+            id="default-toggle"
+            className="sr-only peer"
+          />
+          <div className="w-11 h-6 bg-gray-800 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+          <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+            Oil
+          </span>
+        </label>
+        <br />
+        <label class="inline-flex relative items-center mb-4 cursor-pointer">
+          <input
+            type="checkbox"
+            value=""
+            id="default-toggle"
+            className="sr-only peer"
+          />
+          <div className="w-11 h-6 bg-gray-800 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+          <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+            Flour
+          </span>
+        </label>
+        <br />
+        <label class="inline-flex relative items-center mb-4 cursor-pointer">
+          <input
+            type="checkbox"
+            value=""
+            id="default-toggle"
+            className="sr-only peer"
+          />
+          <div className="w-11 h-6 bg-gray-800 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+          <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+            Sugar
+          </span>
+        </label>
+        <br />
+        <Button
+          onClick={() => setCount((count) => count + 1)}
+          variant="primary"
+          size="lg"
+        >
+          Save ingredients
+        </Button>
+      </div>
+      <div className="container mx-auto py-8 px-8 mt-24 rounded-md bg-blue-100">
         <h1 className="font-bold leading-tight pb-4 text-4xl text-gray-800">
           Foodie Collection
         </h1>
@@ -92,7 +184,7 @@ function App() {
           -
         </Button>
       </div>
-      <div className="container mx-auto py-8 mt-8 px-8 rounded-md bg-blue-100">
+      <div className="container mx-auto py-8 mt-8 px-8 rounded-md bg-gray-300">
         <List />
       </div>
       <div className="container mx-auto py-8 mt-8 px-8 rounded-md bg-blue-100">
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import * as React from 'react';
+import { supabaseClient } from '../supabaseClient';
+import { Product, Recipe } from '../interfaces/api';
 
 const List = () => {
+  const [data, setData] = React.useState<Product[]>([]);
+  const [inputValue, setInputValue] = React.useState<HTMLInputElement>();
+
+  const loadData = async () => {
+    const { data, error } = await supabaseClient.from('products').select();
+
+    if (!error) {
+      setData(data);
+    }
+  };
+
+  React.useEffect(() => {
+    loadData();
+  }, []);
   return (
-    <div>
+    <>
+      <h1>In storage:</h1>
       <ul className="w-48 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
         <li className="py-2 px-4 w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600">
           Profile
         </li>
+        <input type="number" placeholder="Quantity" />
         <li className="py-2 px-4 w-full border-b border-gray-200 dark:border-gray-600">
           Settings
         </li>
@@ -15,7 +33,9 @@ const List = () => {
         </li>
         <li className="py-2 px-4 w-full rounded-b-lg">Download</li>
       </ul>
-    </div>
+
+      <div className="card">{JSON.stringify(data)}</div>
+    </>
   );
 };
 
