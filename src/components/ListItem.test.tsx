a05b71f9032f5e329e5e5b7330f3d0c1e3c2963d
Erikharmens <54328933+Erikharmens@users.noreply.github.com>
2022-08-22 15:42:07 +0200

    added testing-library. commit to commit in case of computer problems

---
+++ src/components/ListItem.test.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect, vi } from 'vitest';
+import '@testing-library/jest-dom';
+
+import ListItem from './ListItem';
+
+describe('ListItem component', () => {
+  const handlePropChange = vi.fn();
+  const handleDelete = vi.fn();
+
+  it('should display given prop values', () => {
+    const { container, getByDisplayValue } = render(
+      <ListItem
+        handlePropChange={handlePropChange}
+        handleDelete={handleDelete}
+        index={0}
+        product={{
+          id: 0,
+          name: 'Corn',
+          qty: 100,
+          unit: 'grams',
+        }}
+      />
+    );
+
+    expect(container).toBeTruthy();
+    expect(getByDisplayValue('Corn')).toBeVisible();
+    expect(getByDisplayValue(100)).toBeInTheDocument();
+  });
+
+  it.todo('should change name when typing in the input');
+  it.todo('should change quantity when typing in the input');
+  it.todo('should set quantity to 0 when clear input');
+  it.todo('should change unit when typing in the input');
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 /// <reference types="vitest" />
+/// <reference types="vite/client" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  test: {},
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
 });
