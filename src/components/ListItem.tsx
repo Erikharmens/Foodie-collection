3791f4aa9bf4d190554a8b027a825944035a2a68
Erikharmens <54328933+Erikharmens@users.noreply.github.com>
2022-08-23 13:40:44 +0200

    working on adding labels to input

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -19,28 +19,39 @@ const ListItem = ({
   index,
 }: Props) => (
   <li>
-    {product.id}
+    <label htmlFor="ingredient" className="mx-2 text-sm text-blue-900">
+      Ingredient:
+    </label>
     <input
-      className="bg-gray-50 border border-gray-300 text-blue-900 text-sm rounded-lg p-2 m-2"
+      id="ingredient"
+      className="bg-gray-50 border border-gray-300 text-blue-900 text-sm rounded-lg p-2 mb-2"
       type="text"
       value={product.name}
       onChange={(event) => handlePropChange(event, index, 'name')}
     />
 
+    <label htmlFor="quantity" className="mx-2 text-sm text-blue-900">
+      Quantity:
+    </label>
     <input
-      className="bg-gray-50 border border-gray-300 text-blue-900 text-sm rounded-lg p-2"
+      id="quantity"
+      className="bg-gray-50 border border-gray-300 text-blue-900 text-sm rounded-lg p-2 mb-2"
       type="number"
       value={product.qty}
       onChange={(event) => handlePropChange(event, index, 'qty')}
     />
+    <label htmlFor="unit" className="mx-2 text-sm text-blue-900">
+      Unit:
+    </label>
     <input
-      className="bg-gray-50 border border-gray-300 text-blue-900 text-sm rounded-lg p-2 w-24 ml-2"
+      id="unit"
+      className="bg-gray-50 border border-gray-300 text-blue-900 text-sm rounded-lg p-2 w-24 mb-2"
       type="text"
       value={product.unit}
       onChange={(event) => handlePropChange(event, index, 'unit')}
     />
     <button
-      className="bg-red-900 px-8 text-gray-200 p-1 rounded ml-2"
+      className=" bg-red-900 px-8 text-gray-200 p-1 rounded ml-2"
       type="button"
       onClick={() => handleDelete(index)}
     >
